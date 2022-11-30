83c529b564f163baf68d9abd1a692639cc763caa
MatteoVandenKerkhove <matteo.vanden.kerkhove@student.ehb.be>
2022-11-30 11:42:18 +0100

    started ex. 1

---
+++ exercises/1-functions/1-1-name.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+
+context.fillRect(20, 50, 100, 50);
